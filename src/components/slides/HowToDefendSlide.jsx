48423c2aa116f79b5cbd5ee986a40258ead7cfe3
Young Ryou <ryouyt@gmail.com>
2026-06-01 03:03:21 +1200

    feat(HowToDefend): add Escaping content

--- src/components/slides/HowToDefendSlide.jsx
+++ src/components/slides/HowToDefendSlide.jsx
@@ -1,9 +1,71 @@
 function HowToDefendSlide() {
   return (
     <div className="slide">
-      <h2>How to Defend</h2>
+      <h2>
+        How to <span className="--blue">Defend</span>
+      </h2>
 
-      <div className="contents"></div>
+      <div className="contents">
+        <div className="contents--flex">
+          <div>
+            <h4>
+              Context-Aware HTML <span className="--green">Escaping</span>
+            </h4>
+            <code className="code-box">
+              <p className="hint">// JS Code Tag e.g.</p>
+              <p className="--green">{`<script>`}</p>
+
+              <div className="spacer" />
+
+              <p className="hint">// Convert HTML special letter to string</p>
+              <p>
+                {`< → `}
+                <span className="--red">{`&lt;`}</span>
+              </p>
+              <p>
+                {`> → `}
+                <span className="--red">{`&gt;`}</span>
+              </p>
+              <p>
+                {`" → `}
+                <span className="--red">{`&quot;`}</span>
+              </p>
+
+              <div className="spacer" />
+
+              <p className="hint">// Using</p>
+
+              <p className="--yellow">.replace</p>
+              <p>
+                <span className="--blue">innerHTML</span> →{' '}
+                <span className="--blue">textContent</span> or{' '}
+                <span className="--blue">innerText</span>
+              </p>
+
+              <div className="spacer" />
+
+              <p className="hint">
+                // React automatically escapes data within curly brackets.
+              </p>
+              <p className="--blue">{`{comment.content}`}</p>
+            </code>
+          </div>
+
+          <div>
+            <h4>
+              HTML <span className="--blue">Sanitization</span>
+            </h4>
+            <code className="code-box"></code>
+          </div>
+
+          <div>
+            <h4>
+              Content Security Policy <span className="--red">(CSP)</span>
+            </h4>
+            <code className="code-box"></code>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/slides/HowToHackingSlide.jsx
+++ src/components/slides/HowToHackingSlide.jsx
@@ -9,35 +9,35 @@ function HowToHackingSlide() {
         <div className="contents--flex">
           <div className="box">
             <h3 className="box-title">
-              1. <span className="--green">Session</span> Hijacking
+              <span className="--green">Session</span> Hijacking
             </h3>
             <p>Stealing session User's Cookie allows to log in.</p>
 
             <img src="src/assets/cookie-image.webp" alt="Cookie Hijacking" />
 
-            <h3 className="--green text--right">Cookie!</h3>
+            <h4 className="--green text--right">Cookie!</h4>
           </div>
 
           <div className="box">
             <h3 className="box-title">
-              2. <span className="--blue">Defacement</span> Attack
+              <span className="--blue">Defacement</span> Attack
             </h3>
             <p>Render fake login or payment forms.</p>
 
             <img src="src/assets/dom-image.webp" alt="DOM Hijacking" />
 
-            <h3 className="--blue text--right">DOM Structure!</h3>
+            <h4 className="--blue text--right">DOM Structure!</h4>
           </div>
 
           <div className="box">
             <h3 className="box-title">
-              3. XSS <span className="--red">Worm</span>
+              XSS <span className="--red">Worm</span>
             </h3>
             <p>Creating an uncontrollable viral loop.</p>
 
             <img src="src/assets/loop-image.webp" alt="XSS Worm" />
 
-            <h3 className="--red text--right">Loop!</h3>
+            <h4 className="--red text--right">Loop!</h4>
           </div>
         </div>
       </div>
