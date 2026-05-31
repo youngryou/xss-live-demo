661989f086f57cb1b6a77680fb81ae2157ace5e7
Young Ryou <ryouyt@gmail.com>
2026-05-31 14:38:32 +1200

    feat(XssDemo): add comment input and payload

--- src/components/XssDemo.jsx
+++ src/components/XssDemo.jsx
@@ -6,20 +6,57 @@ function XssDemo() {
     { id: 2, name: 'Alice', comment: 'This is a normal comment.' },
     { id: 3, name: 'Eve', comment: 'Do you know XSS?' },
   ])
+  const [input, setInput] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const newComment = {
+      id: comments.length + 1,
+      name: 'Anonymous',
+      comment: input,
+    }
+    setComments([...comments, newComment])
+    setInput('')
+  }
 
   return (
     <div>
       <h2>XSS Live Demo</h2>
 
+      {/* Comment List Area */}
       <h3>Comments:</h3>
       <div>
         {comments.map((comment) => (
           <div key={comment.id}>
-            <p>
-              {comment.name}: {comment.comment}
-            </p>
+            <p>{comment.name}:</p>
+            {/* IMPORTANT! DANGEROUS ZONE!
+              Render raw HTML 
+              (bypasses React's default XSS protection, using 'dangerouslySetInnerHTML') 
+            */}
+            <p dangerouslySetInnerHTML={{ __html: comment.comment }} />
           </div>
         ))}
+      </div>
+
+      {/* Comment Input Area */}
+      <form onSubmit={handleSubmit}>
+        <textarea
+          placeholder="Type your comment here... (or copy the below payload)"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Submit Comment</button>
+      </form>
+
+      {/* Payload / Attack JS Code */}
+      {/* <img src="x" onerror="alert('XSS Attack Successful! Session Hijacked!')"> */}
+      <div>
+        <p>Sample Payload:</p>
+        <p>
+          &lt;img src="x" onerror="alert('XSS Attack Successful! Session
+          Hijacked!')"&gt;
+        </p>
       </div>
     </div>
   )
