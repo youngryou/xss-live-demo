791aa92cdea23eb786d4c5d7f16790d4ba3279c4
Young Ryou <ryouyt@gmail.com>
2026-06-04 20:54:32 +1200

    feat(End): minor change

--- src/components/slides/EndSlide.jsx
+++ src/components/slides/EndSlide.jsx
@@ -2,7 +2,7 @@ function EndSlide() {
   return (
     <div className="slide">
       <div className="contents content--center text--center">
-        <p className="--grey">Cross-Site Scripting (XSS)</p>
+        <p className="--grey">SQL Injection vs. Cross-Site Scripting (XSS)</p>
 
         <h2>
           We can <span className="--blue">Defend</span> against{' '}
@@ -13,8 +13,8 @@ function EndSlide() {
           "Writing secure baseline code is what safeguards databases and
           sensitive user privacy."
         </p>
-        <p />
-        <p />
+
+        <div className="spacer" />
 
         <h3 className="--green">Thank You!</h3>
       </div>
