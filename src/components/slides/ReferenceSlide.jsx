c474aafb900a61bc0adda5f990713d6346a36a66
Young Ryou <ryouyt@gmail.com>
2026-06-04 21:28:19 +1200

    feat(Reference): add reference list

--- src/components/slides/ReferenceSlide.jsx
+++ src/components/slides/ReferenceSlide.jsx
@@ -3,7 +3,58 @@ function ReferenceSlide() {
     <div className="slide">
       <h2>Reference</h2>
 
-      <div className="contents"></div>
+      <div className="contents">
+        <ul>
+          <li>
+            <p>
+              <strong className="--blue">OWASP</strong>{' '}
+              <em>Cross Site Scripting (XSS)</em> by KirstenS
+            </p>
+            <p className="hint">https://owasp.org/www-community/attacks/xss/</p>
+          </li>
+          <li>
+            <p>
+              <strong className="--blue">MDN (mozilla.org)</strong>{' '}
+              <em>Cross-site scripting (XSS)</em>
+            </p>
+            <p className="hint">
+              https://developer.mozilla.org/en-US/docs/Web/Security/Attacks/XSS
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong className="--blue">Huntress</strong>{' '}
+              <em>Cross-Site Scripting (XSS) Vulnerability</em> by Monica
+              Burgess
+            </p>
+            <p className="hint">
+              https://www.huntress.com/threat-library/vulnerabilities/cross-site-scripting-xss
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong className="--blue">ManageEngine - Log360</strong>{' '}
+              <em>
+                SQL injection and cross-site scripting: The differences and
+                attack anatomy
+              </em>{' '}
+              by Kritika Sharma
+            </p>
+            <p className="hint">
+              https://www.manageengine.com/log-management/cyber-security/sql-injection-web-application-attack-and-cross-site-scripting-the-differences-and-attack-anatomy.html
+            </p>
+          </li>
+          <li>
+            <p>
+              <strong className="--blue">GeeksforGeeks</strong>{' '}
+              <em>Difference Between XSS and SQL Injection</em>
+            </p>
+            <p className="hint">
+              https://www.geeksforgeeks.org/ethical-hacking/difference-between-xss-and-sql-injection/
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
